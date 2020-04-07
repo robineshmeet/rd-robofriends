cdbc32a451cb6a33c70942ebcb9b187f6d4eb55f
robineshmeet <robineshmeet@gmail.com>
2020-04-07 15:20:14 +0530

    adding files

---
+++ src/robots.js
@@ -0,0 +1,92 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Robin Dhiman',
+    username: 'Dobin',
+    email: 'Robin@man.biz'
+  },
+  {
+    id: 2,
+    name: 'Avneet Kaur',
+    username: 'Kaur-Avneet',
+    email: 'Kaurneet@shekaur.tv'
+  },
+  {
+    id: 3,
+    name: 'Rahul Pandey',
+    username: 'Rpandey',
+    email: 'Pandey@rahul.net'
+  },
+  {
+    id: 4,
+    name: 'Shivam Tyagi',
+    username: 'Tyagishivam',
+    email: 'shivam@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Vanshika Rawat',
+    username: 'Vrawat',
+    email: 'rawar@indica.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dikshant Dhiman',
+    username: 'Mr_dhiman',
+    email: 'dhiman@tcs.info'
+  },
+  {
+    id: 7,
+    name: 'Pooja Chauhan',
+    username: 'Pooja.Chauhan',
+    email: 'Chauhan@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Mounika Karri V',
+    username: 'Karri_mounika',
+    email: 'karri@vizag.me'
+  },
+  {
+    id: 9,
+    name: 'Tejaswani Ponala',
+    username: 'Ponalatejas',
+    email: 'tejas@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Harpreet Kaur',
+    username: 'Kaur.harpreet',
+    email: 'karpreet@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Mohit',
+    username: 'MR.MOHIT',
+    email: 'sirmohit@karina.biz'
+  },
+  {
+    id: 12,
+    name: 'Anand Ducat',
+    username: 'Anand.Ducat',
+    email: 'ducand@mia.biz'
+  },
+  {
+    id: 13,
+    name: 'Sandee Ducat',
+    username: 'deep.sandi',
+    email: 'deeps@newkon.mis'
+  },
+  {
+    id: 14,
+    name: 'Prashant Ducat',
+    username: 'btechshant',
+    email: 'antech@burj.biz'
+  },
+  {
+    id: 15,
+    name: 'Madhur',
+    username: 'Madhur-Ducat',
+    email: 'dudhur@cali.scitx'
+  }
+];
